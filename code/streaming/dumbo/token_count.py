8110e844314a4e51529721ad898b8f37f01f7033
Hadoop  User <hadoop@ubuntu.(none)>
2013-10-17 18:06:52 -0700

    good stopwords

--- code/streaming/dumbo/token_count.py
+++ code/streaming/dumbo/token_count.py
@@ -3,13 +3,22 @@ import nltk
 class Mapper(object):
     
     def __init__(self):
-        with open(self.params["excludes"], "r") as excludes:
-            self.excludes = set(line.strip() for line in excludes)
+        if 'stopwords' in self.params:
+            with open(self.params['stopwords'], 'r') as excludes:
+                self._stopwords = set(line.strip() for line in excludes)
+        else:
+            self._stopwords = None
 
     def __call__(self, key, value):
         for word in nltk.word_tokenize(value):
-            if not word in self.excludes:
+            if not word in self.stopwords:
                 yield word, 1
+
+    @property
+    def stopwords(self):
+        if not self._stopwords:
+            self._stopwords = nltk.corpus.stopwords.words('english')
+        return self._stopwords
 
 def reducer(key, values):
     yield key, sum(values)
