bca437134510a1295e7ddc3f6556a070f81498b0
Benjamin Bengfort <benjamin@bengfort.com>
2013-10-19 13:52:18 -0400

    probabilistic generation

--- code/collocation/generate.py
+++ code/collocation/generate.py
@@ -1,3 +1,5 @@
+import random
+
 from ast import literal_eval
 from nltk import ConditionalFreqDist
 
@@ -13,26 +15,43 @@ class Generator(object):
     def cfdist(self):
         if not self._cfdist:
             self._cfdist = ConditionalFreqDist()
-            for ngram, count in self.ngrams():
+            for ngram, count in self.bigrams():
                 self._cfdist[ngram[0]].inc(ngram[1], count)
         return self._cfdist
 
-    def ngrams(self):
+    def bigrams(self):
         with open(self.output, 'r') as output:
             for line in output:
                 line = line.strip()
                 ngram, count = line.split('\t')
                 yield literal_eval(ngram), int(count)
 
+    def select(self, cond):
+        """
+        Returns a probabilistic selection of the unigram based on the
+        previous unigram in the bigrams set (the cond). The selection is
+        based on the uniform probability of unigrams given the starting
+        unigram.
+        """
+        choices = self.cfdist[cond].items
+        total = sum(w for c, w in choices())
+        r = random.uniform(0, total)
+        upto = 0
+        for c, w in choices():
+            if upto + w > r:
+                return c
+            upto += w
+        assert False
+
     def generate_iter(self, start, length=None):
         length = length or self.length
         for i in xrange(length):
             yield start
-            start = self.cfdist[start].max()
+            start = self.select(start)
 
     def generate(self, start, length=None):
         return " ".join(self.generate_iter(start, length))
 
 if __name__ == "__main__":
     g = Generator("output.txt")
-    print g.generate("will")
+    print g.generate("ahab")
