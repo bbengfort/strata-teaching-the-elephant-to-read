6f5a90e6c0f3d38b4c9dbbcac01472b442fbb45c
Benjamin Bengfort <benjamin@bengfort.com>
2013-10-24 23:31:04 -0400

    better ngram mangement

--- code/collocation/ngram.py
+++ code/collocation/ngram.py
@@ -1,40 +1,44 @@
 import nltk
+import string
 
 from nltk.util import bigrams, trigrams
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import wordpunct_tokenize
+from dumbo import MultiMapper, sumreducer, main
 
-class Mapper(object):
+class CollocationMapper(object):
 
     def __call__(self, key, value):
-        for ngram in self.trigrams(value):
+        for ngram in self.ngrams(value):
             yield ngram, 1
-
-    def bigrams(self, value):
-        for bigram in bigrams(self.tokenize(value)):
-            yield bigram
-
-    def trigrams(self, value):
-        for trigram in trigrams(self.tokenize(value)):
-            yield trigram
 
     def normalize(self, word):
         word = word.lower()
         return word
 
     def tokenize(self, sentence):
-        for word in word_tokenize(sentence):
-            yield self.normalize(word)
+        for word in wordpunct_tokenize(sentence):
+            token = self.normalize(word)
+            if token: yield token
 
-def reducer(key, values):
-    yield key, sum(values)
+    def ngrams(self, value):
+        raise NotImplementedError("Subclasses must implement.")
+
+class BigramMapper(CollocationMapper):
+
+    def ngrams(self, value):
+        for bigram in bigrams(self.tokenize(value)):
+            yield bigram
+
+class TrigramMapper(CollocationMapper):
+
+    def ngrams(self, value):
+        for trigram in trigrams(self.tokenize(value)):
+            yield trigram
 
 def runner(job):
-    job.additer(Mapper, reducer, reducer)
-
-def starter(prog):
-    excludes = prog.delopt("excludes")
-    if excludes: prog.addopt("param", "excludes="+excludes)
+    mapper = BigramMapper()
+    #mapper = TrigramMapper()
+    job.additer(mapper, sumreducer, combiner=sumreducer)
 
 if __name__ == "__main__":
-    import dumbo
-    dumbo.main(runner, starter)
+    main(runner)
