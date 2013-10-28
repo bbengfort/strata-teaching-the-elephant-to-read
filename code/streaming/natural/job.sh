9bf6334c87279c8f76c324fe6de3017828235b4c
Benjamin Bengfort <benjamin@bengfort.com>
2013-10-27 21:32:48 -0400

    notes

--- code/streaming/natural/job.sh
+++ code/streaming/natural/job.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#start hadoop
+#start-all.sh
+
+
 MAPPER=/home/hadoop/tutorial/code/streaming/natural/mapper.py
 REDUCER=/home/hadoop/tutorial/code/streaming/natural/reducer.py
 
@@ -7,3 +11,10 @@ $HADOOP_PREFIX/bin/hadoop jar $HADOOP_PREFIX/contrib/streaming/hadoop-*streaming
     -file $MAPPER -file $REDUCER \
     -mapper $MAPPER -reducer $REDUCER -combiner $REDUCER \
     -input $1 -output $2
+
+
+#input file:
+#/user/hadoop/gutenberg
+
+#output file:
+#/user/hadoop/gutenberg-out
